7272df7b1213cac6dfd457689599c44266bb7c8f
TanyaKolp <lancerchg@gmail.com>
2017-07-21 18:40:10 +0200

    continue

--- src/com/tania/DocTest.java
+++ src/com/tania/DocTest.java
@@ -11,21 +11,8 @@ import java.io.FileOutputStream;
  * Created by tanya on 21.07.17.
  */
 public class DocTest {
-    public void newWordDoc(String filename, String fileContent)
-            throws Exception {
-        XWPFDocument document = new XWPFDocument();
-        XWPFParagraph tmpParagraph = document.createParagraph();
-        tmpParagraph.setAlignment(ParagraphAlignment.CENTER);
-
-        XWPFRun tmpRun = tmpParagraph.createRun();
-        tmpRun.setText(fileContent);
-        tmpRun.setFontSize(18);
-        FileOutputStream fos = new FileOutputStream(new File(filename + ".doc"));
-        document.write(fos);
-        fos.close();
-    }
     public static void main(String[] args) throws Exception {
-        DocTest app = new DocTest();
-        app.newWordDoc("testfile", "Hello World!");
+        AgreementFormCreater app = new AgreementFormCreater();
+        app.createForm(null);
     }
 }
