81448f03bcd9d57a214a86d3d70cdb5390fa521f
TanyaKolp <lancerchg@gmail.com>
2017-07-24 13:59:44 +0200

    doc editor

---
+++ src/com/tania/TestMapper.java
@@ -0,0 +1,13 @@
+package com.tania;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by tanya on 21.07.17.
+ */
+public interface TestMapper {
+    // Sales for accounting report
+    List<LinkedHashMap<String, Object>> getSalesForAccountingReport(Map<Object, Object> paramMap);
+}
