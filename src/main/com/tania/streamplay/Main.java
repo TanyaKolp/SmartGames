ddc3e50a7ff57b8062e565bf5c696419398ce8ea
TanyaKolp <lancerchg@gmail.com>
2017-09-22 15:56:22 +0200

    stream game

--- src/main/com/tania/streamplay/Main.java
+++ src/main/com/tania/streamplay/Main.java
@@ -2,10 +2,13 @@ package com.tania.streamplay;
 
 import com.tania.Data;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -28,24 +31,34 @@ public class Main {
                     System.out.println("anyMatch: " + s);
                     return s.startsWith("A");
                 });
-        System.out.println("result "+b);
+        System.out.println("result " + b);
 
-//        List list = Collections.singletonList("qwer");
-        Data el = new Data("key", "v");
-        List<Data> list =  Collections.singletonList(el);
-//        List list = new ArrayList(Arrays.asList("qwer"));
-        Data  d = list.get(0);
-        d.setValue("new");
-        list.set(0,new Data("key", "set"));
-        System.out.println("Collections.singletonList");
-        System.out.println(list.get(0));
+//cant reuse stream, but can reuse streamSupplier))
+        /**
+         * Stream<String> stream =
+         Stream.of("d2", "a2", "b1", "b3", "c")
+         .filter(s -> s.startsWith("a"));
 
-        List<Data> arr = Arrays.asList(el);
-        Data change = arr.get(0);
-        change.setValue("new");
-//        arr.add(0,"dfds");
+         stream.anyMatch(s -> true);    // ok
+         stream.noneMatch(s -> true);   // exception
+         */
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
 
-        System.out.println("Arrays.asList");
-        System.out.println(arr.get(0));
+        streamSupplier.get().anyMatch(s -> true);   // ok
+        streamSupplier.get().noneMatch(s -> true);  // ok
+        IntSummaryStatistics collect = Stream.of(123, 1, 232, 23432432, 12232, 344, 233, 123)
+                .collect(Collectors.summarizingInt(i -> i));
+        System.out.println(collect);
+        String s = Stream.of("d2", "a2", "b1", "b3", "c").collect(Collectors.joining());
+        String smart = Stream.of("d2", "a2", "b1", "b3", "c").collect(Collectors.joining(" and "));
+        String fullUse = Stream.of("d2", "a2", "b1", "b3", "c").collect(Collectors.joining(" and ", "Bred of ", " ended"));
+      Stream.of(" ", "").distinct();
+        System.out.println(s);
+        System.out.println(smart);
+        System.out.println(fullUse);
+
+
     }
 }
